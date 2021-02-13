75a3618186c01c2025f53cd83c689554e1f80264
rad <rad@pop-os.localdomain>
2021-02-13 21:28:17 +0000

    added some error handing, added happy path unit test

--- shopping_basket/sh_basket.py
+++ shopping_basket/sh_basket.py
@@ -51,13 +51,46 @@ class ShoppingBasket:
 
     """
     def __init__(self, catalogue: dict, offers: dict, basket: dict = {}):
-        # ToDo: add error handling for inputs
-        self.basket = basket
-        self.catalogue = catalogue if catalogue else {}
+        if catalogue:
+            self.__check_catalogue(catalogue)
+        else:
+            raise ValueError('Catalogue can not be empty')
+        self.catalogue = catalogue
+
+        if basket:
+            self.__check_basket(basket)
+        self.basket = basket if basket else {}
+
+        if offers:
+            self.__check_offers(offers)
         self.offers = offers if offers else {}
 
-    def add_basket(self, basket: dict):
+    def set_basket(self, basket: dict):
+        self.__check_basket(basket)
         self.basket = basket if basket else {}
+
+    @staticmethod
+    def __check_catalogue(catalogue: dict):
+        if any([not isinstance(s, str) for s in catalogue.keys()]):
+            raise TypeError('Identifiers for the catalogue must be strings')
+        if any([not isinstance(v, (int, float)) for v in catalogue.values()]):
+            raise TypeError('Values for the catalogue must be int or float')
+
+    def __check_basket(self, basket: dict):
+        if any([not isinstance(s, str) for s in basket.keys()]):
+            raise TypeError('Identifiers for the basket must be strings')
+        if any([not isinstance(v, (int, float)) for v in basket.values()]):
+            raise TypeError('Values for the basket must be int or float')
+        if any([k not in self.catalogue.keys() for k in basket.keys()]):
+            raise KeyError('The basket items must be in the catalogue')
+
+    @staticmethod
+    def __check_offers(offers: dict):
+        if any([not isinstance(s, str) for s in offers.keys()]):
+            raise TypeError('Identifiers for the offers must be strings')
+        if any([not isinstance(v, str) for v in offers.keys()]):
+            raise TypeError('Offers must be stored in dictionaries')
+        # ToDo: add check for *deal* and *discount* keys, they must be present
 
     @staticmethod
     def round(number: float, places: int = 2) -> float:
@@ -71,22 +104,22 @@ class ShoppingBasket:
 
         for item in self.basket.keys():
             # item_total = quantity * price
-            item_total = self.round(self.basket[item] * self.catalogue[item])
+            basket_quantity = self.basket[item]
+            item_total = self.round(basket_quantity * self.catalogue[item])
             sub_total = self.round(sub_total + item_total)
             if item in self.offers.keys():
-                if 'deal' in self.offers[item].keys() and\
-                        self.basket[item] >= int(self.offers[item]['deal'][0]):
-                    item_deal_quantity = self.basket[item] // int(
-                        self.offers[item]['deal'][0])
-                    free_items = (int(self.offers[item]['deal'][0]) - int(
-                        self.offers[item]['deal'][-1]))
+                if 'deal' in self.offers[item].keys() and \
+                        basket_quantity >= int(self.offers[item]['deal'][0]):
+                    deal_quantity = int(self.offers[item]['deal'][0])
+                    item_deal_quantity = basket_quantity // deal_quantity
+                    free_items = deal_quantity - int(self.offers[item]['deal'][-1])
                     item_discount = self.round(
                         (item_deal_quantity * free_items * self.catalogue[item]))
-                    discount = self.round(discount + item_discount)
                 elif 'discount' in self.offers[item].keys():
                     item_discount = self.round(
                         item_total * (self.offers[item]['discount']))
-                    discount = self.round(discount + item_discount)
+
+                discount = self.round(discount + item_discount)
                 del item_discount
 
         total = self.round(sub_total - discount)
--- shopping_basket/shopping_basket_tests/test_shopping_basket.py
+++ shopping_basket/shopping_basket_tests/test_shopping_basket.py
@@ -15,25 +15,51 @@ def test_basket():
     }
     offers = {'baked_beans': {'deal': '3-for-2', 'discount': 0.1},
               'sardines': {'discount': 0.25}}
-    sh_basket = ShoppingBasket(catalogue=catalogue, offers=offers)
+    basket = {'baked_beans': 4, 'biscuits': 1}
+    sh_basket = ShoppingBasket(basket=basket, catalogue=catalogue, offers=offers)
     yield sh_basket
 
 
 class TestShoppingBasket:
-    def test_constructor_example1(self, test_basket):
+    def test_constructor_happy_path(self, test_basket):
+        assert isinstance(test_basket.basket, dict)
+        assert isinstance(test_basket.catalogue, dict)
+        assert isinstance(test_basket.offers, dict)
+        assert all([isinstance(s, str) for s in test_basket.basket.keys()])
+        assert all([isinstance(s, str) for s in test_basket.catalogue.keys()])
+        assert all([isinstance(s, str) for s in test_basket.offers.keys()])
+        assert all([isinstance(v, (int, float))
+                    for v in test_basket.basket.values()])
+        assert all([isinstance(v, (int, float))
+                    for v in test_basket.catalogue.values()])
+        assert all([isinstance(v, dict)
+                    for v in test_basket.offers.values()])
+        for offer in test_basket.offers.keys():
+            assert all(
+                [isinstance(s, str) for s in test_basket.offers[offer].keys()])
+            assert all([isinstance(v, (int, float, str)) for v in
+                        test_basket.offers[offer].values()])
+
+    def test_constructor_errors(self, test_basket):
+        # I would put the error cases here.
+        # The exercise doesn't seem to require this.
+        # I found this easy to deal with.
+        pass
+
+    def test_example1(self, test_basket):
         basket = {'baked_beans': 4, 'biscuits': 1}
-        test_basket.add_basket(basket)
+        test_basket.set_basket(basket)
         sub_total, discount, total = test_basket.get_basket_price()
         assert 5.16 == sub_total
         assert 0.99 == discount
         assert 4.17 == total
 
-    def test_constructor_example2(self, test_basket):
+    def test_example2(self, test_basket):
         basket = {'baked_beans': 2, 'biscuits': 1, 'sardines': 2}
-        test_basket.add_basket(basket)
+        test_basket.set_basket(basket)
         sub_total, discount, total = test_basket.get_basket_price()
         assert 6.96 == sub_total
         assert 1.15 == discount
         assert 5.81 == total
 
-# ToDo: add more cases
+# ToDo: add more cases for edge cases
